f3d9c12ca13ba62fe8bc7a32a32c110d5ef8bf8a
flydog98 <guinnessoverflow@gmail.com>
2022-06-18 10:15:22 +0900

    add question_list

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
+from .forms import QuestionForm
+
 
 # Create your views here.
 def index(request):
@@ -12,6 +14,7 @@ def index(request):
     context = {'question_list': question_list}
     return render(request, 'pybo/question_list.html', context)
 
+
 def detail(request, question_id):
     """
     pybo 내용 출력
@@ -20,6 +23,7 @@ def detail(request, question_id):
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
 
+
 def answer_create(request, question_id):
     """
     pybo 답변 등록
@@ -27,3 +31,11 @@ def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
     return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request):
+    """
+    pybo 질문 등록
+    """
+    form = QuestionForm()
+    return render(request, 'pybo/question_form.html', {'form': form})
